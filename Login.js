92501e628c2007aa35354fad2ac753813d920a0a
Seamus McShane <seamusmcshane3@gmail.com>
2023-11-06 22:26:06 -0600

    nodejs installed + chatGPT changes

---
+++ Login.js
@@ -0,0 +1,19 @@
+// Login.js
+const mysql  =  require('mysql2')
+
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    //event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    // You should replace these with your actual authentication logic.
+    if (username === 'yourusername' && password === 'yourpassword') {
+        // Successful login, you can redirect to another page or perform other actions.
+        alert('Login successful!');
+    } else {
+        const loginError = document.getElementById('login-error');
+        loginError.textContent = 'Invalid username or password';
+        loginError.style.display = 'block';
+    }
+});
