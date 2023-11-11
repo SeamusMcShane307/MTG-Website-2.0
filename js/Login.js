3605449dc2383e196d90784394da4a893e99d629
Seamus McShane <seamusmcshane3@gmail.com>
2023-11-10 23:29:19 -0600

    login information received + require not defined

--- js/Login.js
+++ js/Login.js
@@ -1,26 +1,31 @@
 // Login.js
-const mysql  =  require('mysql2')
+const mysql = require('mysql2')
+const loginForm = document.getElementById('loginForm');
 
-document.getElementById('login-form').addEventListener('submit', function (event) {
-    //event.preventDefault();
+loginForm.addEventListener('submit',  (event) => {
+    // Access the form data
+    const username= document.getElementById('username').value;
+    const password= document.getElementById('password').value;
+    let messages = [];
+    const errorElement = document.getElementById('error')
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const connection = mysql.createConnection({host:"127.0.0.1", port:"3306", database:"card_db"})
-    //const connection = mysql.createConnection({host:"localhost", user:"root", password:"", database:"card_db"})
-    con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    });
+    if (password.length < 5 || password.length > 15){
+        messages.push("The password should be 5-15 characters long")
+    }
 
-    // You should replace these with your actual authentication logic.
-    /*
-    if (username === 'yourusername' && password === 'yourpassword') {
-        // Successful login, you can redirect to another page or perform other actions.
-        alert('Login successful!');
-    } else {
-        const loginError = document.getElementById('login-error');
-        loginError.textContent = 'Invalid username or password';
-        loginError.style.display = 'block';
-    }*/
+    if (messages.length > 0){
+        event.preventDefault();
+        errorElement.innerText = messages.join("/n")
+    }
+
+    //return 'Form submitted!\nName: ' + username + '\nPassowrd: ' + password;
+    // Process the form data (you can replace this with your desired functionality)
+    //alert('Form submitted!\nName: ' + username + '\nPassowrd: ' + password);
+
+    // const connection = mysql.createConnection({ host: "127.0.0.1", port: "3306", database: "card_db" })
+    // //const connection = mysql.createConnection({host:"localhost", user:"root", password:"", database:"card_db"})
+    // connection.connect(function (err) {
+    //     if (err) throw err;
+    //     console.log("Connected!");
+    // });
 });
