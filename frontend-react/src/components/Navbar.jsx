a23ca0f02a9a973575381d94d612d23e757621c8
Seamus McShane <seamusmcshane3@gmail.com>
2023-12-06 14:16:54 -0600

    trying to make logo work

--- frontend-react/src/components/Navbar.jsx
+++ frontend-react/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 export function Navbar() {
     return(
             <header>
-                <a href="home/index.html"></a>
-                <img src="../css/logo.jpg"></img>
+                <a href="home/index.html"/>
+                <img src="../css/logo.jpg" alt="logo"/>
                 <nav className="navbar">
                     <ul>
                         <li><a href="Trade.html"><button className="navButtons">Trade</button></a></li>
