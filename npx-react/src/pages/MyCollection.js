cfd4c2765b00816d848f89dc1112d47b38f68945
Seamus McShane <seamusmcshane3@gmail.com>
2023-12-13 19:29:16 -0600

    autocomplete search bar finished and added to MyCollection page

---
+++ npx-react/src/pages/MyCollection.js
@@ -0,0 +1,12 @@
+import CardSearchBar from '../components/CardSearchBar';
+
+const MyCollection = () => {
+
+    return (
+        <div className="MyCollection">
+            <CardSearchBar/>
+        </div>
+    )
+}
+
+export default MyCollection
